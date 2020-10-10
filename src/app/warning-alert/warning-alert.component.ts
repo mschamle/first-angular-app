d326775bf21229c631f6d63864fa25b33712a523
Marc Schamle <mschamle@gmail.com>
2020-10-10 21:28:49 +0200

    Implement inline warning-alert

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -2,8 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-warning-alert',
-  templateUrl: './warning-alert.component.html',
-  styleUrls: ['./warning-alert.component.css']
+  template: `
+    <p>warning-alert works!</p>
+  `,
+  styles: [
+    `
+      p {
+        padding: 20px;
+        color: black;
+        border: 1px solid red;
+        background-color: indianred;
+      }
+    `
+    ]
 })
 export class WarningAlertComponent implements OnInit {
 
